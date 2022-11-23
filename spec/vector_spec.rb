b6f81d666a27677dfbcbbaa318c34a582d57ea69
Iwan Buetti <iwan.buetti@gmail.com>
2022-11-23 12:10:24 +0100

    initial commit

---
+++ spec/vector_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Vector do
+  it "has a version number" do
+    expect(Vector::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
